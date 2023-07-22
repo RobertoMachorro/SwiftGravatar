e0e82fd15ccc428dba90f9d4ead7281765e90e2e
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2023-07-21 22:29:12 -0400

    Fixed indentation, reformatted.

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftGravatar",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwiftGravatar",
-            targets: ["SwiftGravatar"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftGravatar"),
-        .testTarget(
-            name: "SwiftGravatarTests",
-            dependencies: ["SwiftGravatar"]),
-    ]
+	name: "SwiftGravatar",
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "SwiftGravatar",
+			targets: ["SwiftGravatar"])
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "SwiftGravatar"),
+		.testTarget(
+			name: "SwiftGravatarTests",
+			dependencies: ["SwiftGravatar"])
+	]
 )
