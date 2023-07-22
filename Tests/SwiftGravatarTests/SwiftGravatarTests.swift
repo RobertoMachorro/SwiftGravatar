6ca5240c35e2b272cbb691f601bfa11b911708d2
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2023-07-22 10:59:37 -0400

    Added assertions to JSON profile test.

--- Tests/SwiftGravatarTests/SwiftGravatarTests.swift
+++ Tests/SwiftGravatarTests/SwiftGravatarTests.swift
@@ -2,22 +2,29 @@ import XCTest
 @testable import SwiftGravatar
 
 final class SwiftGravatarTests: XCTestCase {
-	func testExample() throws {
-		// XCTest Documenation
-		// https://developer.apple.com/documentation/xctest
-
-		// Defining Test Cases and Test Methods
-		// https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-	}
-
-	func testParseValidGravatar() async throws {
+	func testParseGravatarProfileData() async throws {
 		// Request JSON Profile Data
 		// Docs: https://en.gravatar.com/site/implement/profiles/json/
 		guard let url = URL(string: "https://en.gravatar.com/632d2f3abe7be4db174da5cb2760f0ae.json") else {
 			return XCTFail("Unable to parse Gravatar URL")
 		}
 		let (data, _) = try await URLSession.shared.data(from: url)
-		let profile = try JSONDecoder().decode(GravatarProfile.self, from: data)
-		print(profile)
+		let profile = try JSONDecoder().decode(GravatarProfileData.self, from: data)
+
+		if let entry = profile.entry.first {
+			// Check Profile for personal info
+			XCTAssertEqual(entry.preferredUsername, "robertomachorro")
+			XCTAssertEqual(entry.profileUrl, "https://gravatar.com/robertomachorro")
+			XCTAssertEqual(entry.name.formatted, "Roberto Machorro")
+
+			// Check Profile for photos / avatars
+			XCTAssertNotNil(entry.photos.first)
+			XCTAssertEqual(entry.photos.first?.type, "thumbnail")
+
+			// Check Profile for social media accounts
+			XCTAssert(entry.accounts.count > 0)
+		} else {
+			XCTFail("No entries to test.")
+		}
 	}
 }
