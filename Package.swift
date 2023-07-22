1e41722a6b2d0ea72ebc78d1aa4356e95bb04ea1
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2023-07-22 10:11:09 -0400

    Added Swift Crypto to Package.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
 	name: "SwiftGravatar",
+	platforms: [
+		.macOS(.v12), .iOS(.v15)
+	],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
 			name: "SwiftGravatar",
 			targets: ["SwiftGravatar"])
 	],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-crypto.git", .upToNextMajor(from: "2.0.0"))
+	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
 		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
-			name: "SwiftGravatar"),
+			name: "SwiftGravatar",
+			dependencies: [
+				.product(name: "Crypto", package: "swift-crypto")
+			]
+		),
 		.testTarget(
 			name: "SwiftGravatarTests",
 			dependencies: ["SwiftGravatar"])
